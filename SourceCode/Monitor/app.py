62cdde4ac8016450350a1d8b6581eee5784db97c
ThePigFarmer <crgwedel@gmail.com>
2022-11-19 15:30:42 -0600

    updatwes

--- SourceCode/Monitor/app.py
+++ SourceCode/Monitor/app.py
@@ -1,19 +1,3 @@
-import sys
-from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
-
-
-class Form(QDialog):
-
-    def __init__(self, parent=None):
-        super(Form, self).__init__(parent)
-        self.setWindowTitle("My Form")
-
-
-if __name__ == '__main__':
-    # Create the Qt Application
-    app = QApplication(sys.argv)
-    # Create and show the form
-    form = Form()
-    form.show()
-    # Run the main Qt loop
-    sys.exit(app.exec())
+import eel as eel
+eel.init('web')
+eel.start('index.html')
